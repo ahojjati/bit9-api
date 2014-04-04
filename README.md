a432d9770daa92a90e559de293184e8e91775aff
blacktop <blacktop@users.noreply.github.com>
2014-04-04 16:00:48 -0700

    Preparing for conversion to PYPI package.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-![bit9-api logo](https://raw.githubusercontent.com/blacktop/bit9-api/master/doc/bit9-590x254.png)
+![bit9-api logo](https://raw.githubusercontent.com/blacktop/bit9-api/master/doc/logo.png)
 
 bit9-api
 ========
 
 Bit9 API for their Cyber Forensics Service
+
+https://www.bit9.com/solutions/cloud-services/cyber-forensics
---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='bit9_api',
+    test_suite="tests",
+    version='1',
+    packages=['bit9', 'bit9.test'],
+    url='https://github.com/blacktop/bit9-api',
+    license='GPLv3',
+    author='blacktop',
+    author_email='',
+    description='Bit9 API for their Cyber Forensics Service',
+    install_requires=[
+        "requests >= 2.2.1",
+        "colorama",
+    ],
+)
