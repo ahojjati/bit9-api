c2c225de70fc4fc73ca97ad454246ca5dfc4a928
blacktop <blacktop@users.noreply.github.com>
2014-04-14 18:58:24 -0700

    Upping Version Number to 1.0.1

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ except ImportError:
 setup(
     name='bit9-api',
     test_suite="tests",
-    version='1.0.0',
+    version='1.0.1',
     packages=['bit9', 'bit9.test'],
     url='https://github.com/blacktop/bit9-api',
     license='GPLv3',
     author='blacktop',
-    author_email='',
+    author_email='dev@blacktop.io',
     description='Bit9 API for their Cyber Forensics Service',
     install_requires=[
         "requests >= 2.2.1",
